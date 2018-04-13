323cbf2bba4ea01a81639827199f9e95d5c5cef4
Gildas HERY <ghery@e1r2p18.42.fr>
2018-04-13 14:44:47 +0200

    cat push

--- minigames.js
+++ minigames.js
@@ -1,34 +1,47 @@
-
-// initialise Phaser
-var game = new Phaser.Game(1200, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
-var bootstate = {
-  preload: function() {
-  },
-  create: function () {
-
-  },
-
-  update: function () {
-
-  }
-
+var config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 800,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
-game.state.add('bootstate', bootstate);
-//ajouter les state
+// initialise Phaser
+var game = new Phaser.Game(config);
 
-game.state.start('bootstate');
+// var bootstate = {
+//   preload: function() {
+//     game.load.image('cat', 'assets/cat_head.png');
+//   },
+//   create: function () {
+//     cat =  game.add.image(100, 0, 'cat');
+//   },
+//
+//   update: function () {
+//
+//   }
+//
+// };
+
+// game.state.add('bootstate', bootstate);
+// //ajouter les state
+//
+// game.state.start('bootstate');
 
 
 function preload() {
-
+  this.load.image('cat', 'assets/cat_head.png');
 }
 
 function create() {
  //create all button use
+ cat =  this.add.image(100, 400, 'cat').setScale(0.5);
 }
 
 
 function update() {
+
 }
