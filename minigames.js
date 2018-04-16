bbc0032fedc82e7460f7986b665c89fdd0ca2589
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-04-16 09:57:13 +0200

    add maj

--- minigames.js
+++ minigames.js
@@ -27,12 +27,37 @@ function preload() {
   game.load.image('cat', 'assets/cat_head.png');
 }
 
+var cat;
+var speed = -10;
+
 function create() {
+ game.physics.startSystem(Phaser.Physics.ARCADE);
  //create all button use
- cat =  game.add.image(0, 350, 'cat').scale.setTo(0.5, 0.5);
+ cat =  game.add.sprite(300, 350, 'cat');
+ cat.scale.setTo(0.5, 0.5)
+ game.physics.enable(cat);
+ cat.body.collideWorldBounds = true;
+ //cat.velocity = new Phaser.Point(10,0);
+ //cat.speed = 10;
+ //cat.body.angle = Math.PI / 2;
+ //cat.angle = 90;
+ cat.body.acceleration.x = speed;
+ cat.body.maxVelocity.x = 40;
+ //cat.body.maxAcceleration.set(10);
+ //cat.body.moveTo(100, 10, Phaser.ANGLE_LEFT);
+ //timer = game.time.create(false);
+ //timer.start();
+
  game.input.addPointer();
+ /*game.time.events.repeat(Phaser.Timer.SECOND * 1, -1, function ()
+ {
+     cat.x -= 10
+ }, this);*/
  game.input.onUp.add(function ()
  {
+     //cat.body.acceleration.x = 20;
+     cat.x += 30;
+     console.log("test");
  }, this);
 }
 
